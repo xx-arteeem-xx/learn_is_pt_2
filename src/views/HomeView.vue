fef7b1a30ce922cbd7a8132499eb7f11cf7aebb2
xx_arteeem_xx <artem.firsov.qwe@gmail.com>
2025-11-28 22:30:09 +0700

    Первая рабочая версия

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script>
+    import Top from "../components/Top.vue";
+    
+    export default{
+        components: { Top },
+        data(){
+            return{
+
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<template>
+    <Top/>
+</template>
+
+<style></style>
